1b970a13277d5ac7c1a979d423d27ea3b73ae69b
Munjal Patel <munjalpatel@gmail.com>
2014-12-22 08:58:19 +0530

    Implemented OAuth flow

--- src/sponline.js
+++ src/sponline.js
@@ -1,4 +1,9 @@
-var validator = require('validator');
+var validator = require('validator'),
+  jwt = require('jwt-simple'),
+  url = require('url'),
+  OAuth2 = require('oauth').OAuth2;
+
+var SP_AUTH_PATH = '/_layouts/15/OAuthAuthorize.aspx';
 
 function SPOnline(options) {
   if (!options) throw new Error('No options specified');
@@ -11,10 +16,48 @@ function SPOnline(options) {
   this._clientSecret = options.clientSecret;
 }
 
-SPOnline.prototype.authenticate = function (options) {
+SPOnline.prototype.authenticate = function (options, callback) {
   if (!options) throw new Error('No options specified');
   if (!options.siteUrl) throw new Error('No site url is specified');
   if (!options.appToken) throw new Error('No app token is specified');
+
+  if (!validator.isURL(options.siteUrl, {
+    protocols: ['https']
+  })) throw new Error(options.siteUrl + ' is not a valid site url');
+
+  var token;
+
+  try {
+    token = eval(jwt.decode(options.appToken, '', true));
+  } catch (err) {
+    throw new Error(options.appToken + ' is not a valid JWT. ' + err.message);
+  }
+
+  var response = {};
+
+  var apptxSender = token.appctxsender.split('@');
+  var spServer = url.parse(options.siteUrl)
+  var resource = apptxSender[0] + '/' + spServer.host + '@' + apptxSender[1];
+  var appId = this._clientId + '@' + apptxSender[1];
+  var appctx = JSON.parse(token.appctx);
+  var tokenServiceUri = url.parse(appctx.SecurityTokenServiceUri);
+  var tokenURL = tokenServiceUri.protocol + '//' + tokenServiceUri.host + '/' + apptxSender[1] + tokenServiceUri.path;
+  var authorizationURL = options.siteUrl + SP_AUTH_PATH;
+
+  oauth2 = new OAuth2(appId, this._clientSecret, '', authorizationURL, tokenURL);
+  oauth2.getOAuthAccessToken(token.refreshtoken, {
+    grant_type: 'refresh_token',
+    refresh_token: token.refreshtoken,
+    resource: resource
+  }, function (err, accessToken, refreshToken, params) {
+    if (err) {
+      return callback('[' + err.statusCode + '] ' + (JSON.parse(err.data)).error_description.replace(/\r\n/g, ' '));
+    }
+
+    response.refreshToken = '';
+
+    callback(response);
+  });
 };
 
 module.exports = SPOnline;
--- src/test/authenticate.js
+++ src/test/authenticate.js
@@ -24,5 +24,52 @@ describe('SPOnline', function () {
         siteUrl: 'https://sponline'
       })).to.throw(/No app token is specified/);
     });
+
+    describe('Site URL', function () {
+      it('should be URL', function () {
+        var spo = new SPO(options);
+        expect(spo.authenticate.bind(spo, {
+          siteUrl: 'sponline',
+          appToken: '1234'
+        })).to.throw(/sponline is not a valid site url/);
+      });
+
+      it('should use HTTPS protocol', function () {
+        var spo = new SPO(options);
+        expect(spo.authenticate.bind(spo, {
+          siteUrl: 'http://sponline.com',
+          appToken: '1234'
+        })).to.throw(/http:\/\/sponline.com is not a valid site url/);
+      });
+    });
+
+    describe('App Token', function () {
+      it('should be a valid JWT', function () {
+        var spo = new SPO(options);
+        expect(spo.authenticate.bind(spo, {
+          siteUrl: 'https://sponline.com',
+          appToken: '1234'
+        })).to.throw(/1234 is not a valid JWT/);
+      });
+    });
+
+    it('should return an error if authentication was not successful', function (done) {
+      this.timeout(5000);
+
+      var spo = new SPO(options);
+      spo.authenticate({
+        siteUrl: 'https://sponline.com',
+        appToken: process.env.SPONLINE_APP_TOKEN
+      }, function (err) {
+        console.log('\t' + err);
+        expect(err).to.be.a('string');
+        expect(err.length).to.be.at.least(1);
+        done();
+      });
+    });
+
+    //    it('should have the Refresh Token in response', function (done) {
+    //      
+    //    });
   });
 });
